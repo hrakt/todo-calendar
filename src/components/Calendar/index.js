9af3882de768bfdbc162aa0d7a1a3647e017e79f
Hrak Torousian <hraktorres@gmail.com>
2021-02-10 10:47:11 -0800

    Create data objects and import

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -6,6 +6,8 @@ import styles from "./Calendar.module.scss"
 import cx from "classnames"
 import DatePicker from "../DatePicker"
 
+import { monthsArr, daysOfWeek } from "../dataObjects"
+
 const Calendar = () => {
   const [currentMonth, setCurrentMonth] = useState() // current month
   const [previousMonth, setPreviousMonth] = useState() // previous month
@@ -14,31 +16,6 @@ const Calendar = () => {
   const [currentDay, setCurrentDay] = useState("") // current day
   const [currentYear, setYear] = useState() // current year
   const [todaysDate] = useState(new Date())
-
-  const monthsArr = [
-    { name: "January", days: 31, index: 1 },
-    { name: "February", days: 28, index: 2 },
-    { name: "March", days: 31, index: 3 },
-    { name: "April", days: 30, index: 4 },
-    { name: "May", days: 31, index: 5 },
-    { name: "June", days: 30, index: 6 },
-    { name: "July", days: 31, index: 7 },
-    { name: "August", days: 31, index: 8 },
-    { name: "September", days: 30, index: 9 },
-    { name: "October", days: 31, index: 10 },
-    { name: "November ", days: 30, index: 11 },
-    { name: "December ", days: 31, index: 12 },
-  ]
-
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ]
 
   useEffect(() => {
     var dd = todaysDate.getDay() // Sunaday is 0
---
+++ src/components/dataObjects.js
@@ -0,0 +1,24 @@
+export const monthsArr = [
+  { name: "January", days: 31, index: 1 },
+  { name: "February", days: 28, index: 2 },
+  { name: "March", days: 31, index: 3 },
+  { name: "April", days: 30, index: 4 },
+  { name: "May", days: 31, index: 5 },
+  { name: "June", days: 30, index: 6 },
+  { name: "July", days: 31, index: 7 },
+  { name: "August", days: 31, index: 8 },
+  { name: "September", days: 30, index: 9 },
+  { name: "October", days: 31, index: 10 },
+  { name: "November ", days: 30, index: 11 },
+  { name: "December ", days: 31, index: 12 },
+]
+
+export const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
